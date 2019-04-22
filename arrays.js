b9a790e628b5f9769d75291b85e9a4d4584f3650
Iris Quintero <godessiris@gmail.com>
2019-04-22 05:47:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = 
 ["snickers", "hundred grand", "kitkat","skittles"];
 
-function addElementToBeginningOfArray(array, element){
+/*function addElementToBeginningOfArray(array, element){
   return [element, ...array];
  }
 
@@ -240,3 +240,46 @@ function removeElementFromBeginningOfArray(array,element){
   })
 })*/
 
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array];
+}
+
+ function destructivelyAddElementToBeginningOfArray (array, element) {
+  array.unshift(element);
+  return array;
+}
+
+ function accessElementInArray(array, index){
+  return array[index];
+}
+
+
+ function addElementToEndOfArray(array, element) {
+  return [...array, element];
+}
+ function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
+
+ function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift();
+  return array;
+}
+
+ function removeElementFromBeginningOfArray(array){
+  return array.slice(1);
+}
+
+ function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
+
+ function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length - 1);
+}
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1);
+}
+ ProTip! Use n and p to navigate between 
