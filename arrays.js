0521635ca827b9935c9e7207e1085b790326c7e3
Iris Quintero <godessiris@gmail.com>
2019-04-22 05:52:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -19,7 +19,8 @@ function destructivelyAddElementToEndOfArray(array,element){
   return array;
 }
 
-function accessElementInArray(){
+function accessElementInArray(array,index){
+  return array[index];
   
 }
 function destructivelyRemoveElementFromBeginningOfArray(array,element){
